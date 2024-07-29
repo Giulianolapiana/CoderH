db84a7537d9dfa224fb3dfe7c977a2cb5d0c2873
Giulianolapiana <giulianolapianam@gmail.com>
2024-07-28 23:48:01 -0300

    agrego de JSON

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const intereses = document.querySelector("#interests");
 const totalADevolver = document.querySelector("#totalAmount");
 const destino = document.querySelector("#destino");
 const montoViaje = document.querySelector("#montoViaje");
+const nombre = document.querySelector("#nombre");
+const apellido = document.querySelector("#apellido");
+
 
 const formulario = document.querySelector("#form");
 const tasa = 0.06;
@@ -13,12 +16,25 @@ const getRamdomId = () => {
     return Math.floor(Math.random() * Date.now()).toString(16)
 };
 
+const createTask = (taskId, taskNombre, taskApellido, taskViaje, taskCuotas) => {
+    return {
+        id: taskId,
+        nombre: taskNombre,
+        apellido: taskApellido,
+        viaje: taskViaje,
+        cuota: taskCuotas
+    }
+};
+
 formulario.addEventListener("submit", (e) => {
     e.preventDefault();
-    const montoPrestamo = (indicarDestino(destino));
+    const precioViaje = (indicarDestino(destino));
     const numeroCuotas = cuotas.value;
 
-    obtenerCuotasPrestamo(montoPrestamo,numeroCuotas);
+    obtenerCuotasPrestamo(precioViaje,numeroCuotas);
+    const datos = createTask(getRamdomId(),nombre.value,apellido.value,precioViaje,numeroCuotas);
+    console.log(datos);
+    guardarStorage('datosviaje', JSON.stringify(datos));
 });
 
 const obtenerCuotasPrestamo = (monto, cuota) => {
@@ -47,7 +63,6 @@ const obtenerTotal = (cuotaPrestamo, cuota) => {
     };
     pintarPrestamo(prestamo);
     }
-    
 };
 
 const pintarPrestamo = (prestamo) => {
@@ -86,11 +101,21 @@ document.getElementById('boton1').addEventListener('click', function(event) {
     indicarDestino(destino);
 });
 
-const createTask = (taskId, taskNombre, taskApellido) => {
-    return {
-        id: taskId,
-        nombre: taskNombre,
-        apellido: taskApellido
-    }
+
+const guardarStorage = (clave,valor) => {
+    localStorage.setItem(clave, valor);
 };
 
+const getTaskStorage = () => {
+    const taskStorage = JSON.parce(localStorage.getItem('datosviaje'));
+    return taskStorage;
+};
+
+const getTask = () => {
+    createTask = getTaskStorage();
+};
+
+document.addEventListener('DOMContentLoaded', getTask);
+
+//guardar todo el array
+/* guardarStorage('datosviaje', JSON.stringify(prestamo)) */
